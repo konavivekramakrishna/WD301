f6787d28ac3a656ab3c696b18523a74e529d773e
vivek <konavivekramakrishna@gmail.com>
2023-08-02 16:28:03 +0530

    changed vite

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   return defineConfig({
-    build: {
-      outDir: 'build',
-    },
     plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
     },
   });
-}; 
+};
