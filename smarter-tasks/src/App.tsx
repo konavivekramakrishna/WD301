d373e3e66746c4c220ee5ad0e393ccffc3afd721
konavivekramakrishna <konavivekramakrishna@gmail.com>
2023-09-06 18:18:18 +0530

    added react router

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,14 +1,29 @@
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import "./App.css";
 import React from "react";
 import TaskApp from "./TaskApp";
 
-function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
-}
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
