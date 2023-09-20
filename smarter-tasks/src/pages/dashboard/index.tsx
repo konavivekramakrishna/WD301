119a90cfe4d6b6ceda396e3234d6584f311a75a2
vivek <konavivekramakrishna@gmail.com>
2023-09-20 13:17:17 +0530

    rm auth token

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -34,6 +34,7 @@ const Dashboard: React.FC = () => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8 mx-auto"
         onClick={() => {
           window.localStorage.removeItem("userData");
+          window.localStorage.removeItem("authToken");
         }}
         id="logout-link"
       >
