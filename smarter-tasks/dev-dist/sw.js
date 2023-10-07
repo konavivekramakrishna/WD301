504a43c94c444411843b54e98bb320b6ceee02c9
vivek <konavivekramakrishna@gmail.com>
2023-10-07 21:45:01 +0530

    changed vite

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.coovq211ee"
+    "revision": "0.491k5do0ov"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
